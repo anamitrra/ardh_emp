1db21658c0cd3579982865c09c6f88898d65dede
anamitrra <32793691+anamitrra@users.noreply.github.com>
2022-03-03 11:50:07 +0530

    logo added

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="css/nav.css">
 
 <nav>
-        <h2>LOGO</h2>
+        <h2>AARADHAYA HEALTH CARE </h2>
         <div class="links">
             <a href="registration.php">Registration</a>
             <a href="view-customers.php">View</a>
